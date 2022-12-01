f86927cbc50a9ae2f760f04511bb295b8b44d70b
VolodymyrOsinchuk <osinchuk28@live.com>
2022-12-01 12:25:15 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 
-const { connectDB } = require("./db/db");
 const app = express();
 
 app.get("/", (req, res) => {
@@ -16,5 +15,4 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log("Server listening on port " + port);
-  connectDB();
 });
