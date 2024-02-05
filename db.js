b4e5c1fe73c5c22d4dbd6144d6d7e2f13e3ef9dc
VolodymyrOsinchuk <osinchuk28@live.com>
2024-02-05 09:03:45 +0100

    update project, create routes for users

--- db.js
+++ db.js
@@ -5,4 +5,6 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
-console.log("🚀 ~ file: db.js:6 ~ supabase", supabase);
+// console.log("🚀 ~ file: db.js:6 ~ supabase.storage", supabase.storage);
+
+module.exports = supabase;
--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello SQL Server" });
+// import data base
+const supabase = require("./db");
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/v1/users", async (req, res) => {
+  const { data, error } = await supabase.from("users").select();
+
+  if (error) {
+    return res.status(404).json({ error });
+  }
+  res.status(200).json({ data });
 });
 
-app.get("/api", (req, res) => {
+app.get("/api/v1/users/:id", async (req, res) => {
+  console.log("req.params.id", req.params.id);
+  const { data, error } = await supabase
+    .from("users")
+    .select()
+    .eq("id", req.params.id);
+
+  if (error) {
+    return res.status(404).json({ error });
+  }
+  res.status(200).json({ data });
+});
+
+app.post("/api/v1/users", async (req, res) => {
+  const { error } = await supabase.from("users").insert({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (error) {
+    res.send(error);
+  }
+  res.status(201).json({ message: "Created User" });
+});
+
+app.get("/api/v1", (req, res) => {
   res.json({ message: "API SQL Server" });
 });
 
