97888a16e88e423ae13d44a7bc037c94ffb1104b
VolodymyrOsinchuk <osinchuk28@live.com>
2024-07-10 10:26:43 +0200

    delete PG from projet

--- db.js
+++ db.js
@@ -4,13 +4,16 @@ require("dotenv").config();
 
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+const sequelize = new Sequelize("verceldb", "default", "L94ptzbACwuB", {
   host: "ep-calm-snow-a2kwy1no-pooler.eu-central-1.aws.neon.tech",
   dialect: "postgres",
-  operatorsAliases: false,
+  dialectOptions: {
+    ssl: true,
+    native: true,
+  },
   logging: false,
   pool: {
-    max: 3,
+    max: 5,
     min: 0,
     acquire: 3000,
     idle: 10000,
